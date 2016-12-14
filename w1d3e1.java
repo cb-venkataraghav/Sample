0acdb34cfda83cf2f8d051f9a9cd70da9c721f7c
cbVenkataRaghav <venkataraghav@chargebee.com>
2016-12-14 11:04:28 +0530

    week 1 --- day 3 --- 4 exercises

---
+++ w1d3e1.java
@@ -0,0 +1,22 @@
+package animals;
+import java.util.Scanner;
+public class w1d3e1{
+
+	public static int no_of_animals;
+	char animal_habitat;
+	char consumer_type;
+	String species_name;
+	Scanner snr = new Scanner(System.in);
+	public w1d3e1(String s_name,char  c_type,char  a_habitat){
+	
+		species_name = s_name;
+		consumer_type = c_type;
+		a_habitat = animal_habitat;
+
+		no_of_animals++;
+
+	}
+
+
+
+}
